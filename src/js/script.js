01ae8c6388c80771dcbc58d54cf724c103f400f3
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2024-08-24 21:22:31 -0300

    feat: funcionalidade e abrir/fechar modal finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 "use strict";
-console;
+// modal 
+const btnModal = document.querySelector('.c-nav__shoppingCart');
+const modal = document.querySelector('.c-modal');
+const shoppingCart = document.querySelector('.c-shoppingCart');
+const cBody = document.body;
+btnModal.addEventListener('click', (ev) => {
+    ev.preventDefault();
+    modal.style.display = 'flex';
+    modal.style.top = `${document.documentElement.scrollTop}px`;
+    cBody.style.overflowY = 'hidden';
+});
+modal.addEventListener('click', (ev) => {
+    modal.style.display = 'none';
+    cBody.style.overflowY = 'scroll';
+    console.log('clicou no pai');
+});
+shoppingCart.addEventListener('click', (ev) => {
+    ev.stopPropagation();
+    console.log('clicou no filho');
+});
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,1 +1,24 @@
-console
+// modal 
+const btnModal = document.querySelector('.c-nav__shoppingCart') as HTMLDivElement;
+const modal = document.querySelector('.c-modal') as HTMLDialogElement;
+const shoppingCart = document.querySelector('.c-shoppingCart') as HTMLDivElement;
+
+const cBody = document.body;
+
+btnModal.addEventListener('click', (ev) => {
+  ev.preventDefault();
+  modal.style.display = 'flex';
+  modal.style.top = `${document.documentElement.scrollTop}px`;
+  cBody.style.overflowY = 'hidden';
+});
+
+modal.addEventListener('click', (ev) => { 
+  modal.style.display = 'none';
+  cBody.style.overflowY = 'scroll';
+  console.log('clicou no pai')
+});
+
+shoppingCart.addEventListener('click', (ev) => {
+  ev.stopPropagation()
+  console.log('clicou no filho')
+})
