0b8ab11479c5899d464d1d20d0969e8707a4d094
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2024-08-26 16:45:33 -0300

    feat: animação dos links do nav finalizado

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,28 @@
 "use strict";
+// marcação dos links do nav
+const cHeader = document.querySelector('#home');
+const cAboutUs = document.querySelector('#aboutUs');
+const navHome = document.querySelector('#nav_home');
+const navAboutUS = document.querySelector('#nav_aboutUs');
+const navCatalog = document.querySelector('#nav_catalog');
+window.addEventListener('scroll', () => {
+    const positionHome = cHeader.offsetHeight;
+    const positionAboutUs = positionHome + cAboutUs.offsetHeight;
+    if (window.scrollY <= positionHome) {
+        navHome.classList.add('active');
+        navAboutUS.classList.remove('active');
+    }
+    else if (window.scrollY > positionHome && window.scrollY <= positionAboutUs) {
+        navHome.classList.remove('active');
+        navAboutUS.classList.add('active');
+        navCatalog.classList.remove('active');
+    }
+    else {
+        navAboutUS.classList.remove('active');
+        navCatalog.classList.add('active');
+    }
+    ;
+});
 // modal 
 const btnModal = document.querySelector('.c-nav__shoppingCart');
 const modal = document.querySelector('.c-modal');
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,8 +1,34 @@
+// marcação dos links do nav
+const cHeader = document.querySelector('#home') as HTMLElement;
+const cAboutUs = document.querySelector('#aboutUs') as HTMLElement;
+const navHome = document.querySelector('#nav_home') as HTMLAnchorElement;
+const navAboutUS = document.querySelector('#nav_aboutUs') as HTMLAnchorElement;
+const navCatalog = document.querySelector('#nav_catalog') as HTMLAnchorElement;
+
+window.addEventListener('scroll', () => {
+  const positionHome = cHeader.offsetHeight;
+  const positionAboutUs = positionHome + cAboutUs.offsetHeight;
+
+  if (window.scrollY <= positionHome) {
+    navHome.classList.add('active');
+    navAboutUS.classList.remove('active');
+
+  } else if (window.scrollY > positionHome && window.scrollY <= positionAboutUs) {
+    navHome.classList.remove('active');
+    navAboutUS.classList.add('active');
+    navCatalog.classList.remove('active');
+
+  } else {
+    navAboutUS.classList.remove('active');
+    navCatalog.classList.add('active');
+  };
+
+});
+
 // modal 
 const btnModal = document.querySelector('.c-nav__shoppingCart') as HTMLDivElement;
 const modal = document.querySelector('.c-modal') as HTMLDialogElement;
 const shoppingCart = document.querySelector('.c-shoppingCart') as HTMLDivElement;
-
 const cBody = document.body;
 
 btnModal.addEventListener('click', (ev) => {
