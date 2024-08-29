e4463f0ff1b21f08d76a8618c35f03dbe539ec30
lucasbarbosadev <lucasbarbosadev1@gmail.com>
2024-08-29 16:43:02 -0300

    feat: NAV sideBar mobile finalizada

--- src/js/script.js
+++ src/js/script.js
@@ -6,8 +6,8 @@ const navHome = document.querySelector('#nav_home');
 const navAboutUS = document.querySelector('#nav_aboutUs');
 const navCatalog = document.querySelector('#nav_catalog');
 window.addEventListener('scroll', () => {
-    const positionHome = cHeader.offsetHeight;
-    const positionAboutUs = positionHome + cAboutUs.offsetHeight;
+    const positionHome = cHeader.offsetHeight - 1;
+    const positionAboutUs = positionHome + cAboutUs.offsetHeight - 1;
     if (window.scrollY <= positionHome) {
         navHome.classList.add('active');
         navAboutUS.classList.remove('active');
@@ -23,7 +23,7 @@ window.addEventListener('scroll', () => {
     }
     ;
 });
-// modal 
+// modal carrinho
 const btnModal = document.querySelector('.c-nav__shoppingCart');
 const modal = document.querySelector('.c-modal');
 const shoppingCart = document.querySelector('.c-shoppingCart');
@@ -35,9 +35,40 @@ btnModal.addEventListener('click', (ev) => {
     cBody.style.overflowY = 'hidden';
 });
 modal.addEventListener('click', (ev) => {
-    modal.style.display = 'none';
-    cBody.style.overflowY = 'scroll';
+    if (ev.target === modal) {
+        modal.style.display = 'none';
+        cBody.style.overflowY = 'scroll';
+    }
 });
-shoppingCart.addEventListener('click', (ev) => {
-    ev.stopPropagation();
+// modal menu
+const btnModalNav = document.querySelector('.c-nav__menu');
+const modalNav = document.querySelector('.c-sideBarNav');
+const modalNavChield = document.querySelector('.c-nav__mobile');
+const btnCloseNav = document.querySelector('.btn-closeNav');
+const navLinksMobile = [...document.querySelectorAll('.c-nav__links--mobile')];
+btnModalNav.addEventListener('click', (ev) => {
+    modalNav.style.top = `${document.documentElement.scrollTop}px`;
+    modalNav.style.display = 'flex';
+    cBody.style.overflowY = 'hidden';
+    modalNavChield.classList.add('openSideBar');
+});
+const closeModal = () => {
+    modalNav.style.display = 'none';
+    cBody.style.overflowY = 'scroll';
+};
+btnCloseNav.addEventListener('click', () => {
+    closeModal();
+    modalNavChield.classList.remove('openSideBar');
+});
+modalNav.addEventListener('click', (ev) => {
+    if (ev.target === modalNav) {
+        closeModal();
+        modalNavChield.classList.remove('openSideBar');
+    }
+});
+navLinksMobile.forEach(item => {
+    item.addEventListener('click', () => {
+        closeModal();
+        modalNavChield.classList.remove('openSideBar');
+    });
 });
--- src/ts/script.ts
+++ src/ts/script.ts
@@ -1,13 +1,13 @@
 // marcação dos links do nav
 const cHeader = document.querySelector('#home') as HTMLElement;
 const cAboutUs = document.querySelector('#aboutUs') as HTMLElement;
-const navHome = document.querySelector('#nav_home') as HTMLAnchorElement;
-const navAboutUS = document.querySelector('#nav_aboutUs') as HTMLAnchorElement;
-const navCatalog = document.querySelector('#nav_catalog') as HTMLAnchorElement;
+const navHome = document.querySelector('#nav_home') as HTMLElement;
+const navAboutUS = document.querySelector('#nav_aboutUs') as HTMLElement;
+const navCatalog = document.querySelector('#nav_catalog') as HTMLElement;
 
 window.addEventListener('scroll', () => {
-  const positionHome = cHeader.offsetHeight;
-  const positionAboutUs = positionHome + cAboutUs.offsetHeight;
+  const positionHome = cHeader.offsetHeight - 1;
+  const positionAboutUs = positionHome + cAboutUs.offsetHeight - 1;
 
   if (window.scrollY <= positionHome) {
     navHome.classList.add('active');
@@ -25,10 +25,10 @@ window.addEventListener('scroll', () => {
 
 });
 
-// modal 
-const btnModal = document.querySelector('.c-nav__shoppingCart') as HTMLDivElement;
-const modal = document.querySelector('.c-modal') as HTMLDialogElement;
-const shoppingCart = document.querySelector('.c-shoppingCart') as HTMLDivElement;
+// modal carrinho
+const btnModal = document.querySelector('.c-nav__shoppingCart') as HTMLElement;
+const modal = document.querySelector('.c-modal') as HTMLElement;
+const shoppingCart = document.querySelector('.c-shoppingCart') as HTMLElement;
 const cBody = document.body;
 
 btnModal.addEventListener('click', (ev) => {
@@ -39,11 +39,43 @@ btnModal.addEventListener('click', (ev) => {
 });
 
 modal.addEventListener('click', (ev) => { 
-  modal.style.display = 'none';
+  if (ev.target === modal){
+    modal.style.display = 'none';
+    cBody.style.overflowY = 'scroll';
+
+  }
+});
+
+// modal menu
+const btnModalNav = document.querySelector('.c-nav__menu') as HTMLElement;
+const modalNav = document.querySelector('.c-sideBarNav') as HTMLElement;
+const modalNavChield = document.querySelector('.c-nav__mobile') as HTMLElement;
+const btnCloseNav = document.querySelector('.btn-closeNav') as HTMLElement;
+const navLinksMobile = [...document.querySelectorAll('.c-nav__links--mobile')] as HTMLElement[];
+
+btnModalNav.addEventListener('click', (ev) => {
+  modalNav.style.top = `${document.documentElement.scrollTop}px`
+  modalNav.style.display = 'flex';
+  cBody.style.overflowY = 'hidden';
+});
+
+const closeModal = (): void => {
+  modalNav.style.display = 'none';
   cBody.style.overflowY = 'scroll';
+}
+
+btnCloseNav.addEventListener('click', () => {
+  closeModal();
 });
 
-shoppingCart.addEventListener('click', (ev) => {
-  ev.stopPropagation()
-});
+modalNav.addEventListener('click', (ev) => {
+  if (ev.target === modalNav) {
+    closeModal();
+  }
+})
 
+navLinksMobile.forEach(item => {
+  item.addEventListener('click', () => {
+    closeModal();
+  })
+})
